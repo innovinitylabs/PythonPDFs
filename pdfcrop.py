f0163651a55c7ddcf984126630c91fee14a8f396
innovinity <rajasekarcmr@icloud.com>
2023-09-18 09:10:22 +0530

    Script used to crop pdf

---
+++ pdfcrop.py
@@ -0,0 +1,17 @@
+from pypdf import PdfReader, PdfWriter, Transformation
+from pypdf.generic import RectangleObject
+
+reader = PdfReader('ml_primer.pdf')
+
+writer = PdfWriter()
+
+
+for page in reader.pages:
+    mb = page.mediabox
+    mb.left, mb.bottom = (120, 70)
+    mb.right, mb.top = (490, 700)
+    page.cropbox = RectangleObject((mb.left, mb.bottom, mb.right, mb.top))
+    page.scale_by(1.5)
+    writer.add_page(page)
+
+writer.write("cropped.pdf")
